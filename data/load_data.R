7314d71bd5a6f1c4b9141c3fee29fe7a84d02105
Steffen Aasen <github.plop432@slmail.me>
2025-03-22 10:19:15 +0100

    Update data loading

--- data/load_data.R
+++ data/load_data.R
@@ -1,27 +1,31 @@
-fnames_study1 <- list.files("data/study1", full.names = T)
-fnames_study2 <- list.files("data/study2", full.names = T)
-fnames_study3 <- list.files("data/study3", full.names = T)
+# Study 1         =======
+fnames_study1 <- list.files(paste0(relative_path,"data/study1"), full.names = T)
 
-map_df(fnames_study1, \(x){
-  read_csv(x)[c(-1,-2),] |>
-    mutate(
-      .before = 1,
-      date = str_extract(x, "(?<=data_)[0-9_]+")
-    )
-}) -> data_study1
+s1 <- list()
+s1[["raw_data"]] <- map_df(fnames_study1, \(x){
+  read_csv(x)[c(-1,-2),] |> mutate(
+    .before = 1,
+    date = str_extract(x, "(?<=data_)[0-9_]+") )
+  })
 
-map_df(fnames_study2, \(x){
-  read_csv(x)[c(-1,-2),] |>
-    mutate(
-      .before = 1,
-      date = str_extract(x, "(?<=data_)[0-9_]+")
-    )
-}) -> data_study2
 
-map_df(fnames_study3, \(x){
-  read_csv(x)[c(-1,-2),] |>
-    mutate(
-      .before = 1,
-      date = str_extract(x, "(?<=data_)[0-9_]+")
-    )
-}) -> data_study3
+# Study 2         ======
+fnames_study2 <- list.files(paste0(relative_path,"data/study2"), full.names = T)
+
+s2 <- list()
+s2[["raw_data"]] <- map_df(fnames_study2, \(x){
+  read_csv(x)[c(-1,-2),] |> mutate(
+    .before = 1,
+    date = str_extract(x, "(?<=data_)[0-9_]+") )
+  })
+
+
+# Study 3         ======
+fnames_study3 <- list.files(paste0(relative_path,"data/study3"), full.names = T)
+
+s3 <- list()
+s3[["raw_data"]] <- map_df(fnames_study3, \(x){
+  read_csv(x)[c(-1,-2),] |> mutate(
+    .before = 1,
+    date = str_extract(x, "(?<=data_)[0-9_]+") )
+  })
