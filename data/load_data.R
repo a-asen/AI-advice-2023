07428d513b9a9570ad851b7987ba750c72279e1b
Steffen Aasen <github.plop432@slmail.me>
2025-04-20 08:34:39 +0200

    Update load data

--- data/load_data.R
+++ data/load_data.R
@@ -1,8 +1,8 @@
 # Study 1         =======
 fnames_study1 <- list.files(paste0(relative_path,"data/study1"), full.names = T)
 
-s1 <- list()
-s1[["raw_data"]] <- map_df(fnames_study1, \(x){
+raw_data <- list()
+raw_data[["s1"]] <- map_df(fnames_study1, \(x){
   read_csv(x)[c(-1,-2),] |> mutate(
     .before = 1,
     date = str_extract(x, "(?<=data_)[0-9_]+") )
@@ -12,8 +12,7 @@ s1[["raw_data"]] <- map_df(fnames_study1, \(x){
 # Study 2         ======
 fnames_study2 <- list.files(paste0(relative_path,"data/study2"), full.names = T)
 
-s2 <- list()
-s2[["raw_data"]] <- map_df(fnames_study2, \(x){
+raw_data[["s2"]] <- map_df(fnames_study2, \(x){
   read_csv(x)[c(-1,-2),] |> mutate(
     .before = 1,
     date = str_extract(x, "(?<=data_)[0-9_]+") )
@@ -23,8 +22,7 @@ s2[["raw_data"]] <- map_df(fnames_study2, \(x){
 # Study 3         ======
 fnames_study3 <- list.files(paste0(relative_path,"data/study3"), full.names = T)
 
-s3 <- list()
-s3[["raw_data"]] <- map_df(fnames_study3, \(x){
+raw_data[["s3"]] <- map_df(fnames_study3, \(x){
   read_csv(x)[c(-1,-2),] |> mutate(
     .before = 1,
     date = str_extract(x, "(?<=data_)[0-9_]+") )
