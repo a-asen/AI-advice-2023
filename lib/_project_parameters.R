60aebe49c3de84bbb58f7fbc3b23725eb55ffa1f
Steffen Aasen <github.plop432@slmail.me>
2025-05-01 18:18:43 +0200

    Update project parameters

--- lib/_project_parameters.R
+++ lib/_project_parameters.R
@@ -1,37 +1,57 @@
 #'                    **PROJECT PARAMETERS**                              ======
+message("Setting project options...")
 
 #' Set general project parameters
 options(
   # Get current date/time
   project_date_time = format( Sys.time(), "_%Y-%m-%d_%H-%M-%S_"),
 
-  # Figure related settings
+  #' **Data related settings **
+  project_save_data_to_file = TRUE,
+  project_save_data_location = "data/",
+  project_save_data_formats = c(".RData"),
+  project_save_data_with_date_time = FALSE,
+
+  #' **Figure related settings**
   project_save_figs_to_file = TRUE,
-  project_save_figs_location = "outputs/figs/",
+  project_save_figs_location = "figs/",
   project_save_figs_formats = c(".svg", ".jpeg"),
   project_save_figs_with_date_time = FALSE,
 
-  # Table related settings
-  project_save_tbls_location = "outputs/tbls/",
+  #' **Table related settings**
   project_save_tbls_to_file = TRUE,
-  project_save_tbls_formats = c(".html", ".docx"),
+  project_save_tbls_location = "tbls/",
+  project_save_tbls_formats = c(".html"),
   project_save_tbls_with_date_time = FALSE,
 
-  # Aesthetics:
-  project_custom_ggplot = TRUE,
-  project_custom_ggplot_colours = c("#F8766D", "#619CFF", "#f8ea6d"),
-  project_custom_ggplot_colours2 = c("#619CFF","#0046bc","#F8766D","#b20b00"),
-
-  # Bayesian options:
+  #' **Bayesian related settings**
   project_bayes_run_models = FALSE,
   project_bayes_save_to_file = FALSE,
   project_bayes_save_with_date_time = TRUE,
-  project_bayes_diagnostics = TRUE,
+  project_bayes_diagnostics = FALSE,
 
   mc.cores = 6,   #' or as many as you have
   brms.backend = "cmdstanr",
 
 
+  #' ** Preferred colours**
+  project_custom_colours = list(
+    `-r` = "#ffd6d2", r = "#F8766D", R = "#b20b00", `+R` = "#8c0000",
+    `-b` = "#d6e9ff", b = "#78ADFF", B = "#00348C", `+B` = "#00246D",
+    `-g` = "#c9ffd6", g = "#00BA38", G = "#006400", `+G` = "#004B00",
+    `-o` = "#ffe0c2", o = "#FF9E4A", O = "#B25000", `+O` = "#8C3D00",
+    `-y` = "#fffbd6", y = "#FFE359", Y = "#D1B000", `+Y` = "#AA8F00",
+    `-p` = "#e9d6ff", p = "#B476FF", P = "#7436B3", `+P` = "#5A1A99"
+  ),
+  # project_custom_colours_rrR      = c("#ffd6d2", "#F8766D", ),
+  # project_custom_colours_rgy      = c("#F8766D", "#619CFF", "#f8ea6d"),
+  # project_custom_colours_pale_bBrR = c("#4477AA","#114477","#EE6677","#991122"),
+
+  #' **GGplot**
+  project_custom_ggplot = TRUE,
+
+
+  #
   #' Because partial matching exists, warn whenever such an instance occur,
   #' as this can be especially problematic for statistical analysis.
   warnPartialMatchArgs = TRUE,
@@ -42,19 +62,4 @@ options(
 #' If conditions can take vectors but only evaluate the first on,
 #' this will add a warning.
 Sys.setenv("_R_CHECK_LENGTH_1_CONDITION_"=1)
-
-# Custom colours      =====
-#'  custom colours for ggplot (if enabled)
-if( getOption("project_custom_ggplot") ){
-  options(
-    ggplot2.continous.colour = getOption("project_custom_ggplot_colours"),
-    ggplot2.continous.fill   = getOption("project_custom_ggplot_colours"),
-    ggplot2.discrete.colour  = getOption("project_custom_ggplot_colours"),
-    ggplot2.discrete.fill    = getOption("project_custom_ggplot_colours")
-  )
-  theme_set( theme_bw() )
-}
-
-
-
 
