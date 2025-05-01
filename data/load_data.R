a5f78376b43d708de86463e1b7d47822e1f3b455
Steffen Aasen <github.plop432@slmail.me>
2025-05-01 18:22:59 +0200

    Add load_data

--- data/load_data.R
+++ data/load_data.R
@@ -1,8 +1,7 @@
 # Study 1         =======
-fnames_study1 <- list.files(paste0(relative_path,"data/study1"), full.names = T)
+fnames_study1 <- list.files(paste0(relative_path,"data/preference"), full.names = T)
 
-raw_data <- list()
-raw_data[["s1"]] <- map_df(fnames_study1, \(x){
+raw_data[["pref"]] <- map_df(fnames_study1, \(x){
   read_csv(x)[c(-1,-2),] |> mutate(
     .before = 1,
     date = str_extract(x, "(?<=data_)[0-9_]+") )
@@ -10,9 +9,9 @@ raw_data[["s1"]] <- map_df(fnames_study1, \(x){
 
 
 # Study 2         ======
-fnames_study2 <- list.files(paste0(relative_path,"data/study2"), full.names = T)
+fnames_study2 <- list.files(paste0(relative_path,"data/identification"), full.names = T)
 
-raw_data[["s2"]] <- map_df(fnames_study2, \(x){
+raw_data[["detect"]] <- map_df(fnames_study2, \(x){
   read_csv(x)[c(-1,-2),] |> mutate(
     .before = 1,
     date = str_extract(x, "(?<=data_)[0-9_]+") )
@@ -20,9 +19,9 @@ raw_data[["s2"]] <- map_df(fnames_study2, \(x){
 
 
 # Study 3         ======
-fnames_study3 <- list.files(paste0(relative_path,"data/study3"), full.names = T)
+fnames_study3 <- list.files(paste0(relative_path,"data/quality"), full.names = T)
 
-raw_data[["s3"]] <- map_df(fnames_study3, \(x){
+raw_data[["quality"]] <- map_df(fnames_study3, \(x){
   read_csv(x)[c(-1,-2),] |> mutate(
     .before = 1,
     date = str_extract(x, "(?<=data_)[0-9_]+") )
