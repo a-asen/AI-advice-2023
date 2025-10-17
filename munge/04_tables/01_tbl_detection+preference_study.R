e945c6121a4c986dd4653e2f9f6c8e8d5c3ebb86
Steffen Aasen <github.plop432@slmail.me>
2025-10-17 15:04:23 +0200

    Update table generation scripts

--- munge/04_tables/01_tbl_detection+preference_study.R
+++ munge/04_tables/01_tbl_detection+preference_study.R
@@ -1,26 +1,26 @@
 
 tbls[["indentification+preference"]] <-
   mod.detect |>
-  bayes_tbl_sum(apa_table = T, coef_calc = "plogis") |>
+  bayes_tbl_sum(apa_table = T, add_loo_R2 = T, coef_calc = "plogis") |>
   bayes_tbl_add_sig() |>
   rename_with(~paste0("det_", .x), c(-group, -var)) |>
   mutate(var = case_when(
-    var   == "Intercept"         ~ "GPT3.0"
+    var   == "true_answerGPT3.0" ~ "GPT3.0"
     , var == "true_answerGPT3.5" ~ "GPT3.5"
     , var == "true_answerGPT4.0" ~ "GPT4.0"
     , var == "true_answerHuman"  ~ "Human"
-    , str_starts(var, "Sigma ") ~ "Sigma (Subject)"
+    , str_starts(var, "Sigma ")  ~ "Sigma (Subject)"
     , T ~ var
   )) |>
   left_join(
     mod.pref |>
-      bayes_tbl_sum(apa_table = T, coef_calc = "plogis") |>
+      bayes_tbl_sum(apa_table = T, coef_calc = "plogis", add_loo_R2 = T) |>
       bayes_tbl_add_sig() |>
       rename_with(~paste0("pref_", .x), c(-group, -var)) |>
       mutate(var = case_when(
-        var   == "Intercept"   ~ "GPT3.0"
-        , var == "modelGPT3.5" ~ "GPT3.5"
-        , var == "modelGPT4.0" ~ "GPT4.0"
+        var   == "modelGPT3.0"      ~ "GPT3.0"
+        , var == "modelGPT3.5"      ~ "GPT3.5"
+        , var == "modelGPT4.0"      ~ "GPT4.0"
         , str_starts(var, "Sigma ") ~ "Sigma (Subject)"
         , T   ~ var
       ))
@@ -31,7 +31,10 @@ tbls[["indentification+preference"]] <-
   tab_spanner("Identification study", starts_with("det_")) |>
   tab_spanner("Preference study", starts_with("pref_")) |>
   cols_move(e, det_p) |>
-  tab_bayes_generics(pre_footnote = "Leave-one-out (LOO) R² and R² may not be accurate for ordinal modals,
-  as they assume the dependent variable to be continuous." ) |>
-  fmt_missing() |>
-  conditional_save("Mod-Bayes--Identification and preference study summary")
+  tab_bayes_generics() |>
+  fmt_missing()
+
+conditional_save(
+  tbls[["indentification+preference"]]
+  , "Mod-Bayes--Identification and preference study summary"
+)
--- munge/04_tables/02_tbl_quality_study.R
+++ munge/04_tables/02_tbl_quality_study.R
@@ -1,49 +1,54 @@
 
+
 tbls[["advice_quality"]] <-
-  bayes_tbl_sum(mod.quality$help, apa_table = T) |>
+  mod.quality$appr |>
+  bayes_tbl_sum(apa_table = T, add_loo_R2 = T) |>
   bayes_tbl_add_sig() |>
-  rename_with(~paste0("help_", .x), c(-group, -var)) |>
+  rename_with(~paste0("appr_", .x), c(-group, -var)) |>
   mutate(var = case_when(
-    var == "SourceGPT3.5" ~ "GPT3.5"
-    , var == "SourceGPT4" ~ "GPT4.0"
-    , var == "SourceHuman"  ~ "Human"
+    var   == "SourceGPT3"   ~ "GPT3.0"
+    , var == "SourceGPT3.5" ~ "GPT3.5"
+    , var == "SourceGPT4"   ~ "GPT4.0"
     , str_starts(var, "Sigma\\ \\(Res") ~ "Sigma (Subject)"
     , T ~ var
   )) |>
   left_join(
-    bayes_tbl_sum(mod.quality$eff , apa_table = T) |>
+    mod.quality$eff |>
+      bayes_tbl_sum(apa_table = T, add_loo_R2 = T) |>
       bayes_tbl_add_sig() |>
       rename_with(~paste0("eff_", .x), c(-group, -var)) |>
       mutate(var = case_when(
-        var == "SourceGPT3.5" ~ "GPT3.5"
-        , var == "SourceGPT4" ~ "GPT4.0"
-        , var == "SourceHuman"  ~ "Human"
+        var   == "SourceGPT3"   ~ "GPT3.0"
+        , var == "SourceGPT3.5" ~ "GPT3.5"
+        , var == "SourceGPT4"   ~ "GPT4.0"
         , str_starts(var, "Sigma\\ \\(Res") ~ "Sigma (Subject)"
         , T ~ var
       ))
     , by = c("group", "var")
   ) |>
   left_join(
-    bayes_tbl_sum(mod.quality$appr , apa_table = T) |>
+    mod.quality$help |>
+      bayes_tbl_sum(apa_table = T, add_loo_R2 = T) |>
       bayes_tbl_add_sig() |>
-      rename_with(~paste0("appr_", .x), c(-group, -var)) |>
+      rename_with(~paste0("help_", .x), c(-group, -var)) |>
       mutate(var = case_when(
-        var == "SourceGPT3.5" ~ "GPT3.5"
-        , var == "SourceGPT4" ~ "GPT4.0"
-        , var == "SourceHuman"  ~ "Human"
+        var   == "SourceGPT3"   ~ "GPT3.0"
+        , var == "SourceGPT3.5" ~ "GPT3.5"
+        , var == "SourceGPT4"   ~ "GPT4.0"
         , str_starts(var, "Sigma\\ \\(Res") ~ "Sigma (Subject)"
         , T ~ var
       ))
     , by = c("group", "var")
   ) |>
   left_join(
-    bayes_tbl_sum(mod.quality$sens , apa_table = T) |>
+    mod.quality$sens |>
+      bayes_tbl_sum(apa_table = T, add_loo_R2 = T) |>
       bayes_tbl_add_sig() |>
       rename_with(~paste0("sens_", .x), c(-group, -var)) |>
       mutate(var = case_when(
-        var == "SourceGPT3.5" ~ "GPT3.5"
-        , var == "SourceGPT4" ~ "GPT4.0"
-        , var == "SourceHuman"  ~ "Human"
+        var   == "SourceGPT3"   ~ "GPT3.0"
+        , var == "SourceGPT3.5" ~ "GPT3.5"
+        , var == "SourceGPT4"   ~ "GPT4.0"
         , str_starts(var, "Sigma\\ \\(Res") ~ "Sigma (Subject)"
         , T ~ var
       ))
@@ -51,12 +56,14 @@ tbls[["advice_quality"]] <-
   ) |>
   mutate(e = "") |>
   gt(groupname_col = "group") |>
-  tab_spanner("Helpfullness", starts_with("help_")) |>
-  tab_spanner("Effectiveness", starts_with("eff_")) |>
   tab_spanner("Appropriateness", starts_with("appr_")) |>
+  tab_spanner("Effectiveness", starts_with("eff_")) |>
+  tab_spanner("Helpfulness", starts_with("help_")) |>
   tab_spanner("Sensitivity", starts_with("sens_")) |>
   tab_bayes_generics(pre_footnote = "
   Leave-one-out (LOO) R² and R² may not be accurate for ordinal modals,
   as they assume the dependent variable to be continuous. ") |>
-  fmt_missing() |>
+  fmt_missing()
+
+tbls[["advice_quality"]] |>
   conditional_save("Mod-Bayes--Quality study summary")
