937324bd3482aeb1cff20359111c5fe8f505d1bf
Steffen Aasen <github.plop432@slmail.me>
2025-10-17 15:04:06 +0200

    Update figure generation scripts

--- munge/03_figures/01_fig_preference.R
+++ munge/03_figures/01_fig_preference.R
@@ -1,12 +1,18 @@
-
 figs[["Advice_preference"]] <-
-  plot(marginal_effects(mod.pref), plot=FALSE)[[1]] +
-  labs(y = "Probability of choosing AI", x = "Model") +
+  conditional_effects(mod.pref)$model |>
+  as_tibble() |>
+  ggplot(aes(effect1__, estimate__)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=lower__, ymax=upper__), width = .2) +
+  geom_hline(yintercept=.5, linetype="dotted", col="red") +
+  labs(y = "Preferance Over Human Answer", x = "") +
   scale_y_continuous(breaks = seq(.3,1,.05), labels = paste0(seq(30,100, 5), " %")) +
   coord_cartesian(ylim = c(.5,1)) +
-  geom_hline(yintercept=.5, linetype="dotted", col="red")
+  theme(axis.text.x.bottom = element_text(size = 10))
 
 conditional_save(
   figs[["Advice_preference"]]
-  , name = "Preference for AI vs. Human advice")
+  , name = "Preference for AI vs. Human advice"
+)
+
 
--- munge/03_figures/03_fig_quality.R
+++ munge/03_figures/03_fig_quality.R
@@ -1,32 +1,37 @@
-
+  # Data figure   ======
 figs[["Quality_of_advice"]] <-
   data[["quality"]] |>
   pivot_longer(c(Helpfulness, Effectiveness, Appropriateness, Sensitivity)) |>
   mutate(Rating = as.numeric(value),
          Source = fct_relevel(Source, "Human")) |>
   ggplot(aes(Source, Rating, col=name)) +
-  scale_colour_manual(values=gen_col("brgo")) +
-  stat_summary(fun.data =  mean_cl_boot, position = position_dodge(.4)) +
-  labs(col = "Category")
+  scale_colour_manual(values = gen_col("brgo")) +
+  stat_summary(fun.data = mean_se, position = position_dodge(.4), linewidth = .2) +
+  stat_summary(fun.data = mean_cl_boot, geom = "errorbar", width = .4, position = position_dodge(.4)) +
+  stat_summary(aes(group = name), fun.data = mean_cl_boot, geom = "line", position = position_dodge(.4), alpha = .5) +
+  labs(col = "Category", x = "") +
+  theme(axis.text.x.bottom = element_text(size = 11))
 
 conditional_save(
   figs[["Quality_of_advice"]]
-  , name = "Quality of the Advice")
+  , name = "Quality of the Advice"
+)
 
 
+# Model figure      ======
 figs[["Quality_of_advice--model"]] <-
-  marginal_effects(mod.quality$help)$Source |>
+  conditional_effects(mod.quality$help)$Source |>
   as_tibble() |>
   bind_rows(
-    marginal_effects(mod.quality$eff)$Source |>
+    conditional_effects(mod.quality$eff)$Source |>
       as_tibble()
   ) |>
   bind_rows(
-    marginal_effects(mod.quality$appr)$Source |>
+    conditional_effects(mod.quality$appr)$Source |>
       as_tibble()
   ) |>
   bind_rows(
-    marginal_effects(mod.quality$sens)$Source |>
+    conditional_effects(mod.quality$sens)$Source |>
       as_tibble()
   ) |>
   mutate(category = case_when(
@@ -36,14 +41,17 @@ figs[["Quality_of_advice--model"]] <-
     Sensitivity     == 1 ~ "Sensitivity" )
     , effect1__ = fct_relevel(effect1__, "Human")
   ) |>
-  ggplot(aes(effect1__, estimate__, col = category)) +
+  ggplot(aes(effect1__, estimate__, col = category, group = category)) +
   geom_point(position = position_dodge(.4)) +
+  geom_line(position = position_dodge(.4), alpha = .5) +
   scale_colour_manual(values=gen_col("brgo")) +
   geom_errorbar(aes(ymin=lower__, ymax=upper__), width = .4,
                  position = position_dodge(.4)) +
-  labs(y = "Rating", x = "Sources", col = "Category")
+  labs(y = "Rating", x = "", col = "Category") +
+  theme(axis.text.x.bottom = element_text(size = 11))
+
 
 conditional_save(
   figs[["Quality_of_advice--model"]]
-  , name = "Quality of the Advice--model")
-
+  , name = "Quality of the Advice--model"
+)
